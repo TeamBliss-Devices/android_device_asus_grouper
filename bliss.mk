cd061d598793b8996af7b1991fde0068c6f09353
electrikjesus <electrikjesus@gmail.com>
2015-07-15 08:49:12 -0400

    Added viper configs

--- bliss.mk
+++ bliss.mk
@@ -21,5 +21,8 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := asus
 
+#Bliss Viper Audio Mod
+BLISS_AUDIO_MOD := viper
+
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:5.1/LMY47D/1743759:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 5.1 LMY47D 1743759 release-keys"
